81eaac30b56bb964a06d401bde0978432199c589
Franz Buse <franz.buse@tu-ilmenau.de>
2019-11-12 13:06:30 +0100

    added rest_plus

--- restplus/restplus.py
+++ restplus/restplus.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restplus import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-@api.route('/hello')
+@api.route('/allItems')
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'item': str(todos)}
+
+todos = {}
+
+@api.route('/<string:todo_id>')
+class ToDoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+    
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
 
 
 if __name__ == '__main__':
