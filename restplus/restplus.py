069ed6b80fb101c99edf89a429f35e284743302d
Franz Buse <franz.buse@tu-ilmenau.de>
2019-11-18 14:20:04 +0100

    added SQLAlchemy

--- restplus/restplus.py
+++ restplus/restplus.py
@@ -1,25 +1,32 @@
-from flask import Flask, request
-from flask_restplus import Resource, Api
+from flask import Flask, request, jsonify
+from flask_restplus import Resource, Api, fields
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config ['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "item.db"
+
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Float)
+    description = db.Column(db.String(100))
 
 @api.route('/allItems')
-class HelloWorld(Resource):
+class AllItems(Resource):
     def get(self):
-        return {'item': str(todos)}
+        return {'item': str(items)}
 
-todos = {}
+items = {}
 
-@api.route('/<string:todo_id>')
+@api.route('/<string:item_id>')
 class ToDoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+    def get(self, item_id):
+        return {item_id: items[item_id]}
     
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    def put(self, item_id):
+        items[item_id] = request.form['data']
+        return {item_id: items[item_id]}
